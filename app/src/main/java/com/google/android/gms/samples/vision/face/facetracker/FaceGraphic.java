157ca69a57eb700004c460d21af608ac4a3a79b4
Oleh Ponomarenko <oleh.ponomarenko@easternpeak.com>
2019-07-19 18:06:15 +0300

    Initial commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceGraphic.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gms.samples.vision.face.facetracker;
+
+import android.graphics.Canvas;
+
+import com.google.android.gms.samples.vision.face.facetracker.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.face.Face;
+
+/**
+ * Graphic instance for rendering face position, orientation, and landmarks within an associated
+ * graphic overlay view.
+ */
+class FaceGraphic extends GraphicOverlay.Graphic {
+    private FaceDetected faceDetected;
+    private volatile Face mFace;
+
+    FaceGraphic(GraphicOverlay overlay, FaceDetected faceDetected) {
+        super(overlay);
+        this.faceDetected = faceDetected;
+    }
+
+    /**
+     * Updates the face instance from the detection of the most recent frame.  Invalidates the
+     * relevant portions of the overlay to trigger a redraw.
+     */
+    void updateFace(Face face) {
+        mFace = face;
+        postInvalidate();
+    }
+
+    /**
+     * Draws the face annotations for position on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        Face face = mFace;
+        if (face == null) {
+            return;
+        }
+        faceDetected.onFaceDetected();
+    }
+
+    interface FaceDetected {
+        void onFaceDetected();
+    }
+
+}
